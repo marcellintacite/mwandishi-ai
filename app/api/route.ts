609d729cf3e3324f109f56577c47eac2793b0157
Aksanti Bahiga Tacite <tacite.bahiga@gmail.com>
2024-03-14 11:06:50 +0200

    Testing api

--- app/api/route.ts
+++ app/api/route.ts
@@ -5,5 +5,5 @@ export async function GET(req: NextRequest) {
   // make a path to the public/tmp folder , the above arrow give : C:\Users\hp\Desktop\mwandishi-ai\.next\server\app\api
   // format the path to be : C:/Users/hp/desktop/
   const path = __dirname.split("\\").slice(0, -4).join("/") + "/";
-  return NextResponse.json({ message: "Hello, World!", path: path });
+  return NextResponse.json({ message: "Hello, World!", path: __dirname });
 }
